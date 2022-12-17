bcb91971fda72d2c13431f7e5cabf94e95d36f34
yosh <imyosh4@gmail.com>
2022-12-17 12:02:13 +0200

    added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config()
 
 const port = process.env.PORT || 8868
 app.use(fileUpload())
-app.use(cors())
+app.use(cors({ origin: 'http://localhost:3000' }))
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true, limit: '50' }))
 
